e3337668c52736c00007c09c8153ca72e7c938c6
alexmortali <alexmortali1@gmail.com>
2020-02-12 19:18:51 +0000

    Began acounts app with login+sign up functionality

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from accounts.views import logout, login, registration
+
+urlpatterns = [
+    url(r'^logout/', logout, name="logout"),
+    url(r'^login/', login, name="login"),
+    url(r'^register/', registration, name="registration"),
+]
