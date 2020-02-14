2eb5751a0a714d677b7659c5a5fbe75a62063754
alexmortali <alexmortali1@gmail.com>
2020-02-14 19:18:11 +0000

    Created products app + models for product+category

---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    """ Model for Categories. Products can be placed in different 
+    categories and features to show on home page """
+
+    title = models.CharField(max_length=250)
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    """ Model for Products """
+
+    name = models.CharField(max_length=250, default='Product')
+    image = models.ImageField(upload_to='products', blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    summary = models.TextField(max_length=200, verbose_name="Summary")
+    description = models.TextField(max_length=1000, verbose_name="Description")
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=9.99)
+
+    def __str__(self):
+        return self.name
