1fee4fd9b991737cc0c4d7f5c9adea9c069a0881
alexmortali <alexmortali1@gmail.com>
2020-02-21 21:45:57 +0000

    Created singlecategoryview

--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
-from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
 from .forms import QuantityForm, SizeForm
 from .models import Category, Product
 # Create your views here.
@@ -18,6 +18,22 @@ class All_Products(ListView):
 
     def get_queryset(self):
         return self.queryset
+
+class SingleCategoryView(SingleObjectMixin, All_Products):
+    """ View inheriting from All_products to show products within a 
+        single category """
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Category.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.object
+        return context
+
+    def get_queryset(self):
+        return self.object.product_set
 
 class ProductDetailView(DetailView):
     """ Single Product view, uses detail view to show a single product """
