df1b3bbe426e9a0edcbef1871fb8bd16145a406d
alexmortali <alexmortali1@gmail.com>
2020-02-21 15:05:43 +0000

    Adjusted all products and product detail views

--- products/views.py
+++ products/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from django.views.generic import ListView
 from .forms import QuantityForm, SizeForm
 from .models import Category, Product
 # Create your views here.
 
-def all_products(request):
-    products = Product.objects.all()
-    return render(request, "categories.html", {"products": products, "category": "All Products"})
+class All_Products(ListView):
+    """ List view for all products """
 
-def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug)
-    return render(request, "product_detail.html", {"product": product})
+    template_name = "products/categories.html"
+    queryset = Product.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = 'All Products'
+        return context
+
+    def get_queryset(self):
+        return self.queryset
+
+class ProductDetailView(DetailView):
+    """ Single Product view, uses detail view to show a single product """
+
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form1'] = QuantityForm()
+        context['form2'] = SizeForm()
+        return context
