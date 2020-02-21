79fd5071121f13ca64e090171a41a742e5bd49eb
alexmortali <alexmortali1@gmail.com>
2020-02-21 21:42:03 +0000

    Created context.py for nav categories

---
+++ products/context.py
@@ -0,0 +1,8 @@
+from .models import Category
+
+
+def category_list(request):
+    """ Returns categories for Navbar """
+    
+    categories = Category.objects.all()
+    return {'categories': categories}
