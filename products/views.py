4b56ccb60df66bfe2f25f71cc6d5b3e6e574f42b
alexmortali <alexmortali1@gmail.com>
2020-02-21 14:36:01 +0000

    Created all products and product_detail view

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .forms import QuantityForm, SizeForm
+from .models import Category, Product
 # Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, "categories.html", {"products": products, "category": "All Products"})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, "product_detail.html", {"product": product})
