b21bbb735d8bcbd9bc007398e1a54eed5c720fe4
alexmortali <alexmortali1@gmail.com>
2020-02-27 14:13:29 +0000

    Edited products urls to correctly use slug

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from .views import All_Products, SingleCategoryView, ProductDetailView
+from . import views
 app_name = 'products'
 
 urlpatterns = [
-    url('', All_Products.as_view(), name='all_products'),
-    url(r'category/<slug>', SingleCategoryView.as_view(), name='category'),
-    url(r'product/<slug>', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^$', views.list_of_products, name='list_of_products'),
+    url(r'^(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail'),
+    url(r'^category/(?P<category_slug>[-\w]+)/$', views.list_of_products_by_category, name='list_of_products_by_category')
 ]
