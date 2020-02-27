2ffa2881dbfd2185335cf31655ce13049ec4e653
alexmortali <alexmortali1@gmail.com>
2020-02-27 14:11:36 +0000

    Edited Category return reverse to correct html

--- products/models.py
+++ products/models.py
@@ -5,10 +5,10 @@ from django.shortcuts import reverse
 
 class Category(models.Model):
     """ Model for Categories. Products can be placed in different 
-    categories and features to show on home page """
+    categories and featured to show on home page """
 
-    slug = models.SlugField(max_length=150, unique=True)
     title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
     featured = models.BooleanField(default=False)
     image = models.ImageField(upload_to='products', blank=True)
 
@@ -18,7 +18,7 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
     
     def get_absolute_url(self):
-        return reverse('products:category', args=[self.slug])
+        return reverse('products:list_of_products_by_category', args=[self.slug])
 
     def __str__(self):
         return self.title
@@ -26,10 +26,10 @@ class Category(models.Model):
 class Product(models.Model):
     """ Model for Products """
     
-    slug = models.SlugField(max_length=150, unique=True)
     name = models.CharField(max_length=150, default='Product')
+    slug = models.SlugField(max_length=150, unique=True)
+    category = models.ForeignKey(Category)
     image = models.ImageField(upload_to='products/media/category_images', blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     summary = models.TextField(max_length=200, verbose_name="Summary")
     description = models.TextField(max_length=1000, verbose_name="Description")
     price = models.DecimalField(max_digits=6, decimal_places=2, default=9.99)
