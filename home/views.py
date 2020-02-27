badadb06f83d0de008de94b31891f7b9160539d3
alexmortali <alexmortali1@gmail.com>
2020-02-27 14:09:16 +0000

    Added doc string to functions

--- home/views.py
+++ home/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from products.models import Category
 
 # Create your views here.
 def index(request):
     """ A view that displays the index page """
-    
     return render(request, "index.html")
 
 def about(request):
+    """ A view that displays the index page """
     return render(request, "about.html")
 
 def contact(request):
+    """ A view that displays the index page """
     return render(request, "contact.html")
