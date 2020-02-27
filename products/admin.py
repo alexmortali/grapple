693497e1dd4f647e8d0c9003922dc1c6f6e6c4f7
alexmortali <alexmortali1@gmail.com>
2020-02-27 14:10:06 +0000

    Added pre populated fields for slug

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Category, Product 
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product, PostAdmin)
