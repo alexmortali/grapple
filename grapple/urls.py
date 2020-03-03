d97aecab3ec06ab8cd9b3cb3be94eb5288de87c6
alexmortali <alexmortali1@gmail.com>
2020-03-03 21:31:05 +0000

    imported cart urls

--- grapple/urls.py
+++ grapple/urls.py
@@ -19,6 +19,7 @@ from home.views import index, about, contact
 from accounts import urls as urls_accounts
 from products import urls as urls_products
 from search import urls as urls_search
+from cart import urls as urls_cart
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -30,5 +31,6 @@ urlpatterns = [
     url(r'^accounts/', include(urls_accounts)),
     url(r'^products/', include(urls_products, namespace='products')),
     url(r'^search/', include(urls_search)),
+    url(r'^cart/', include(urls_cart)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
