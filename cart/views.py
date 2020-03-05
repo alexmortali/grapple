36f0be82ffbea6daad17dfb47a1071791a5c06b7
alexmortali <alexmortali1@gmail.com>
2020-03-05 15:50:08 +0000

    Added delete from cart function

--- cart/views.py
+++ cart/views.py
@@ -40,3 +40,13 @@ def adjust_cart(request, id):
     
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, id):
+    """ Delete a specific product from the cart """
+
+    cart = request.session.get('cart', {})
+    cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
