90f589107030a5b2d438abda52d394a86f852d37
alexmortali <alexmortali1@gmail.com>
2020-03-06 16:53:21 +0000

    Began views for checkout app to test urls

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import BillingForm
+from .models import BillingAddress, OrderLineItem
+from django.conf import settings
+from django.utils import timezone
+from products.models import Product
+import stripe
 
 # Create your views here.
+
+@login_required()
+def checkout(request):
+    """  """
+    form = BillingForm
+
+    context = {'form': form}
+
+    #if request.method == "POST":
+    #    if form.is_valid():
+    #        billing_address = form.save(commit=False)
+    #        billing_address.date = timezone.now()
+    #        billing_address.user = request.user
+    #        billing_address.save()
+    #        return redirect('checkout:payment')
+
+    return render(request, 'checkout.html', context)
+
+def payment(request):
+
+    return render(request, 'payment.html')
