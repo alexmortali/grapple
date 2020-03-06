cce170d87985e1bb20b780ed0d18108e2f0d646e
alexmortali <alexmortali1@gmail.com>
2020-03-06 16:51:53 +0000

    Registered checkout models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BillingAddress, OrderLineItem
 
 # Register your models here.
+admin.site.register(BillingAddress)
+admin.site.register(OrderLineItem)
