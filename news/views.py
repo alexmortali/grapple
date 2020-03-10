a540ff47bf5061078d5d9d6a991252edd2fab9e2
alexmortali <alexmortali1@gmail.com>
2020-03-10 17:24:27 +0000

    Created news views

--- news/views.py
+++ news/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 # Create your views here.
+
+def news_home(request):
+    posts = Post.objects.all().order_by('-created')
+    template = 'news_home.html'
+    context = {'posts': posts}
+    return render(request, template, context)
+
+def news_article(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    template = 'news_article.html'
+    context = {'post': post}
+    return render(request, template, context)
