a814d93ed266f7d8d52a8a8bccfc00f87693c4ba
alexmortali <alexmortali1@gmail.com>
2020-03-10 17:23:26 +0000

    Added news urls

--- grapple/urls.py
+++ grapple/urls.py
@@ -21,6 +21,7 @@ from products import urls as urls_products
 from search import urls as urls_search
 from cart import urls as urls_cart
 from checkout import urls as urls_checkout
+from news import urls as urls_news
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -34,5 +35,6 @@ urlpatterns = [
     url(r'^search/', include(urls_search)),
     url(r'^cart/', include(urls_cart)),
     url(r'^checkout/', include(urls_checkout)),
+    url(r'^news/', include(urls_news)),
     url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
